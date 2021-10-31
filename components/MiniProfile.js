89fd7fe3c669bd7d647478e5f792509a3ab7a0ec
Yasin Khan <yasinkhan500@gmail.com>
2021-10-30 17:11:54 -0700

    fixed miniprofile height placement

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -5,14 +5,14 @@ export default function MiniProfile() {
   const { data: session } = useSession();
 
   return (
-    <div className='flex items-center justify-between mt-14 ml-10'>
+    <div className='flex items-center justify-between mt-7 ml-10'>
       <img
         className='w-16 h-16 rounded-full border p-[2px]'
         src={session?.user?.image}
         alt=''
       />
 
-      <div clasName='flex-1 mx-4'>
+      <div className='flex-1 mx-4'>
         <h2 className='font-bold'>{session?.user?.username}</h2>
         <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
       </div>
--- components/Suggestions.js
+++ components/Suggestions.js
@@ -13,7 +13,7 @@ export default function Suggestions() {
     setSuggestions(suggestions);
   }, []);
   return (
-    <div className='mt-4 ml-10'>
+    <div className='mt-28 ml-10'>
       <div className='flex justify-between text-sm mb-5'>
         <h3 className='text-sm font-bold text-gray-400'>Suggestions for you</h3>
         <button className='text-gray-600 font-semibold'>See All</button>
